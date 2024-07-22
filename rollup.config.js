f2a3944da857b82d6c98c9645e8b3a196a24bdff
iskl <10909568@qq.com>
2024-07-22 22:45:34 +0800

    0.1.1

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,27 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    file: "dist/index.js",
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
   },
-  plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
-};
+  {
+    input: "src/operators/index.ts",
+    output: {
+      file: "dist/operators/index.js",
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      // typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+];
