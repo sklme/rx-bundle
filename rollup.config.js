cce9f80277056ff9ff061837b736383cbe5dcdf0
iskl <10909568@qq.com>
2024-07-22 22:20:02 +0800

    chore: init

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/bundle.js",
+  },
+  plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+};
